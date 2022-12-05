f84fe069d5fe52f8b6a178e9dfb1a8b6ce247023
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-05 08:01:41 -0600

    Refactor useAuth hook to add TypeScript

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
+import { IUser } from '../models/user';
+import { IUserCredentials } from '../models/userCredentials';
 import loginService from '../services/login';
 
 export const useAuth = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<IUser | null>(null);
 
   useEffect(() => {
-    const user = JSON.parse(window.localStorage.getItem('user'));
+    const userValue = window.localStorage.getItem('user');
+    const user = userValue ? JSON.parse(userValue) : null;
     if (user) {
+      console.log('User was found in localStorage: ', user);
       setUser(user);
     }
   }, []);
 
-  async function logIn(credentials) {
+  async function logIn(credentials: IUserCredentials) {
     try {
       const user = await loginService.login(credentials);
       if (user) {
