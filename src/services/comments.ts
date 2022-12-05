5d38458e17f32c5fc51d91786eb4613ceb188989
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-04 20:11:04 -0600

    Refactor comments service to add TypeScript

--- src/services/comments.ts
+++ src/services/comments.ts
@@ -1,19 +1,24 @@
-import HttpClient from './http';
+import { ICommentFromAPI } from '../models/commentFromAPI';
+import { ICommentToAPI } from '../models/commentToAPI';
+import { get, post } from './http';
 
-const http = new HttpClient();
-
-export async function getCommentsByGame(gameId) {
-  const gameComments = await http.get(
-    process.env.REACT_APP_GLITCH_BASE_URL + `/comments?gameId=${gameId}`
+export async function getCommentsByGame(gameId: string) {
+  const gameComments = await get<ICommentFromAPI[]>(
+    `${process.env.REACT_APP_GLITCH_BASE_URL}/comments?gameId=${gameId}`
   );
 
   return gameComments;
 }
 
-export async function createComment(comment) {
-  const newComment = await http.post(
-    process.env.REACT_APP_GLITCH_BASE_URL + '/comments',
-    comment
+export async function createComment(comment: ICommentToAPI) {
+  const newComment = await post<ICommentToAPI, ICommentFromAPI>(
+    `${process.env.REACT_APP_GLITCH_BASE_URL}/comments`,
+    comment,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
   );
 
   return newComment;
